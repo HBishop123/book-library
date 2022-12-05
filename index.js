9bb9c68e6d6125984b506323c7819c1517876242
Harrison Bishop <harrisonbishsp@gmail.com>
2022-12-05 14:02:58 +0000

    added a function that opens and closes the form if its already open, when clicking add book

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 let myLibrary = [];
-let book = document.getElementById('book1');
+let book = document.getElementById('book-container');
 
 let bookLoop = function(){
     for(i = 0; i < myLibrary.length; i++){
-       book.innerHTML = myLibrary
+      book.innerHTML = libraryChanged
     }
 }
 
 
-
-
+//constructor
 class Book {
     constructor(title, author, pages, read){
         this.title = title;
@@ -21,12 +20,42 @@ class Book {
 
 }
 
-let firstBook = new Book('Shawhank-Redemption', 'Stephen King', 1, false)
-myLibrary.push(firstBook)
-console.log(firstBook)
-console.log(myLibrary)
+
+function addBookToLibrary() {}
 
 
-bookLoop()
 
-function addBookoLibrary() {}
+
+
+
+
+// let firstBook = new Book('Shawhank-Redemption', 'Stephen King', 1, false)
+// myLibrary.push(firstBook)
+
+// let secondBook = new Book('Gulag', 'solcheniztyn', 200, true )
+// myLibrary.push(secondBook)
+
+// bookLoop()
+
+
+
+
+
+//JS for the form
+
+
+//bookNumber allows for closing of Add Book with reclick. Function pops add book form
+bookNumber = 0;
+function addBook(){
+    let form = document.getElementById('book-form')
+
+    if ((form.style.display = "hidden") && (number == 0)){
+        form.style.display = "block"
+        bookNumber++
+    }else if(number > 0){
+        form.style.display = "none"
+        bookNumber = 0
+    }
+}
+    
+
