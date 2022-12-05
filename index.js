6e469cc1164162e1cad856937d08a46c783bda76
Harrison Bishop <harrisonbishsp@gmail.com>
2022-12-05 11:36:08 +0000

    added constructor and class for the books

---
+++ index.js
@@ -0,0 +1,21 @@
+let myLibrary = [];
+
+
+
+
+
+
+class Book {
+    constructor(title, author, pages, read){
+        this.title = title;
+        this.author = author;
+        this.pages = 0;
+        this.read = false;
+    }
+
+
+}
+
+function addBookoLibrary() {
+
+}
